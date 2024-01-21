d3722d8e9f29f28a145de34faaa24f8c1fb5225d
Jr G <jg@example.com>
2024-01-20 21:33:51 -0600

    asdfas

---
+++ main.tf
@@ -0,0 +1,50 @@
+resource "aws_eks_cluster" "example" {
+  name     = "example"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+  }
+
+  depends_on = [aws_cloudwatch_log_group.example, aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy, aws_iam_role_policy_attachment.example-AmazonEKSVPCResourceController]
+
+  enabled_cluster_log_types = ["api", "audit"]
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+
+}
+
+# resource "aws_subnet" "example1" {
+  
+# }
+
+# resource "aws_subnet" "example2" {
+  
+# }
+
+resource "aws_iam_role" "example" {
+  name               = "eks-cluster-example"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
+# Optionally, enable Security Groups for Pods
+# Reference: https://docs.aws.amazon.com/eks/latest/userguide/security-groups-for-pods.html
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.example.name
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  # The log group name format is /aws/eks/<cluster-name>/cluster
+  # Reference: https://docs.aws.amazon.com/eks/latest/userguide/control-plane-logs.html
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7
+
+  # ... potentially other configuration ...
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "value"
   type = string
 }
+
+variable "cluster_name" {
+  description = "value"
+  type = string
+}
